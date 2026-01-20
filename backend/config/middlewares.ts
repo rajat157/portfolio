ac08d8cebe99db44f600763ddd4ee824a5ea993c
Rajat Kumar R <rajat157@gmail.com>
2026-01-20 15:34:56 +0530

    Strapi init.

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,28 +1,8 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-    },
-  },
+  'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,12 +1,1 @@
-export default ({ env }) => ({
-  upload: {
-    config: {
-      providerOptions: {
-        localServer: {
-          maxage: 300000,
-        },
-      },
-      sizeLimit: 250 * 1024 * 1024, // 250MB
-    },
-  },
-});
+export default () => ({});
